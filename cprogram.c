8bbc3dfeae7c5e3547cf750d1c6d967d406c39c7
qasanjiv <qesanjiv@gmail.com>
2020-05-13 10:39:31 +0000

    My  Next commit!

--- cprogram.c
+++ cprogram.c
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include<stdio.h>
 int main()
 
@@ -7,12 +6,4 @@ print("Updated");
 printf(“Hello! I am C-Program. Thank you!”);
 return 0;
 
-=======
-lude<stdio.h>
-
-int main(){
-
-printf(“Hello! I am C-Program. Thank you!”);
-return 0;
->>>>>>> b8de1dff3b2060b13b6ab9ec15a0173a8fcaa607
-}
+} 
